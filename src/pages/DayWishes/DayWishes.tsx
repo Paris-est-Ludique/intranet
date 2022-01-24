bf649aa040d9f310c7b249f27f1758e63838ba91
memeriau <manuel.emeriau@mappy.com>
2022-01-24 23:45:10 +0100

    add component for d day informations

--- src/pages/DayWishes/DayWishes.tsx
+++ src/pages/DayWishes/DayWishes.tsx
@@ -6,6 +6,7 @@ import { AppThunk } from "../../store"
 import { fetchVolunteerDayWishesSetIfNeed } from "../../store/volunteerDayWishesSet"
 import { selectUserJwtToken } from "../../store/auth"
 import DayWishes from "../../components/VolunteerBoard/DayWishes/DayWishes"
+import DDayInformations from "../../components/VolunteerBoard/DDayInformations/DDaysInformations"
 import styles from "./styles.module.scss"
 
 export type Props = RouteComponentProps
@@ -19,6 +20,7 @@ const HomePage: FC<Props> = (): JSX.Element => {
             <div className={styles.dayWishPage}>
                 <div className={styles.dayWisContent}>
                     <DayWishes />
+                    <DDayInformations />
                 </div>
             </div>
         )
