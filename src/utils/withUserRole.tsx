458df713add9af717bfe9f48417ef07946facb95
memeriau <manuel.emeriau@mappy.com>
2022-04-16 15:14:42 +0200

    return empty div instead of null when user do not have role for hydrate issue

--- src/utils/withUserRole.tsx
+++ src/utils/withUserRole.tsx
@@ -3,9 +3,9 @@ import { useSelector } from "react-redux"
 import { selectUserRoles } from "../store/auth"
 
 function withUserRole<T>(requiredRole: string, Component: React.ComponentType<T>) {
-    return (props: T): JSX.Element | null => {
+    return (props: T): JSX.Element => {
         const roles = useSelector(selectUserRoles)
-        return roles.includes(requiredRole) ? <Component {...props} /> : null
+        return roles.includes(requiredRole) ? <Component {...props} /> : <div />
     }
 }
 
