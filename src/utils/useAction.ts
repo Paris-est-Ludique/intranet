43b68d427a2f26a26a96bae7029660e36cd12887
memeriau <manuel.emeriau@mappy.com>
2022-04-18 00:21:33 +0200

    add useMemo to useAction

--- src/utils/useAction.ts
+++ src/utils/useAction.ts
@@ -1,11 +1,16 @@
 import { useDispatch } from "react-redux"
+import { useMemo } from "react"
 
 const useAction = (action: (...args: any[]) => any): any => {
     const dispatch = useDispatch()
 
-    return (...args: any[]) => {
-        dispatch(action(...args))
-    }
+    return useMemo(
+        () =>
+            (...args: any[]) => {
+                dispatch(action(...args))
+            },
+        [dispatch, action]
+    )
 }
 
 export default useAction
